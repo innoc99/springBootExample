85c3c04537c2fe5d1b8513acea4081f72dfafb88
illy.kim <illy.kim@daumkakao.com>
2015-08-29 20:02:21 +0900

    HelloController 인테그레이션 테스트 추가

---
+++ src/test/java/com/daumkakao/localcontents/test/websupport/controller/HelloControllerIT.java
@@ -0,0 +1,50 @@
+package com.daumkakao.localcontents.test.websupport.controller;
+
+import com.daumkakao.localcontents.test.websupport.application.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by innoc on 15. 8. 29..
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class HelloControllerIT {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("######"+port);
+        this.base = new URL("http://localhost:"+port+"/");
+        template = new TestRestTemplate();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+    }
+}
