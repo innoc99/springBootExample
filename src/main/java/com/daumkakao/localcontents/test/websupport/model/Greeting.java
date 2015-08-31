54a4bb1a02160cc9d3aaa95c791368ef785c5c5d
illy.kim <illy.kim@daumkakao.com>
2015-08-31 13:22:55 +0900

    Rest 샘플 컨트롤러

---
+++ src/main/java/com/daumkakao/localcontents/test/websupport/model/Greeting.java
@@ -0,0 +1,22 @@
+package com.daumkakao.localcontents.test.websupport.model;
+
+/**
+ * Created by innoc on 15. 8. 29..
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
