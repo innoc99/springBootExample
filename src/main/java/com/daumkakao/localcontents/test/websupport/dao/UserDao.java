7b74727fd56ac7993b306847193aa2f05fd0b350
illy <44281234>
2015-10-25 18:24:43 +0900

    UserDao 최초버전 추가

---
+++ src/main/java/com/daumkakao/localcontents/test/websupport/dao/UserDao.java
@@ -0,0 +1,72 @@
+package com.daumkakao.localcontents.test.websupport.dao;
+
+import model.User;
+
+import java.sql.*;
+
+/**
+ * Created by illy on 2015. 10. 25..
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/test", "root","fhemqb21");
+        PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+
+    public void del(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/test", "root","fhemqb21");
+        PreparedStatement ps = c.prepareStatement("DELETE FROM users WHERE id=?");
+        ps.setString(1, id);
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/test", "root","fhemqb21");
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("illy.kim");
+        user.setName("김종원");
+        user.setPassword("illy");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+
+        dao.del(user.getId());
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,49 @@
+package model;
+
+import com.google.common.base.MoreObjects;
+
+/**
+ * Created by illy on 2015. 10. 25..
+ */
+public class User {
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("password", password)
+                .toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(new User());
+    }
+}
