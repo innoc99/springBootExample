a4fff952e7229e55ea0ec1de777284153a0dd747
illy <44281234>
2016-01-08 02:39:57 +0900

    트랜잭션매니저 적용

--- src/main/java/com/daumkakao/localcontents/test/websupport/service/UserService.java
+++ src/main/java/com/daumkakao/localcontents/test/websupport/service/UserService.java
@@ -4,8 +4,13 @@ import com.daumkakao.localcontents.test.websupport.dao.UserDao;
 import model.Level;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
@@ -17,19 +22,36 @@ public class UserService {
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
-    @Autowired
+
     UserDao userDao;
 
-    public void upgradeLevels(){
-        List<User> users = userDao.getAll();
-        for (User user : users) {
-            if(canUpgradeLevel(user)){
-                upgradeLevel(user);
-            }
+    DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            List<User> users = userDao.getAll();
+            users.stream().filter(this::canUpgradeLevel).forEach(this::upgradeLevel);
+            transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
+            throw e;
         }
     }
 
-    private void upgradeLevel(User user) {
+    protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
     }
--- src/test/java/com/daumkakao/localcontents/test/websupport/service/UserServiceTest.java
+++ src/test/java/com/daumkakao/localcontents/test/websupport/service/UserServiceTest.java
@@ -11,12 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.daumkakao.localcontents.test.websupport.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static com.daumkakao.localcontents.test.websupport.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * UserService JUnit 테스트
@@ -25,6 +30,25 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DaoFactory.class)
 public class UserServiceTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    static class TestUserServiceException extends RuntimeException {
+    }
+
+    static class TestUserService extends UserService {
+        private String id;
+
+        public TestUserService(String id) {
+            this.id = id;
+        }
+
+        protected void upgradeLevel(User user){
+            if(user.getId().equals(this.id)) throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
 
     @Autowired
     private UserService userService;
@@ -42,16 +66,16 @@ public class UserServiceTest {
     @Before
     public void setUp(){
         users = Arrays.asList(
-                new User("innoc1", "김1", "123", Level.BASIC, 49, 0),
-                new User("innoc2", "김2", "1234", Level.BASIC, 50, 10),
-                new User("innoc3", "김3", "1234", Level.SILVER, 60, 29),
-                new User("innoc4", "김4", "1234", Level.SILVER, 60, 30),
-                new User("innoc5", "김5", "1235", Level.GOLD, 100, 100)
+                new User("innoc1", "김1", "123", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER - 1, 0),
+                new User("innoc2", "김2", "1234", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 10),
+                new User("innoc3", "김3", "1234", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD - 1),
+                new User("innoc4", "김4", "1234", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
+                new User("innoc5", "김5", "1235", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
 
     @Test
-    public void upgradeLevels(){
+    public void upgradeLevels() throws Exception {
         userDao.deleteAll();
         for (User user : users) {
             userDao.add(user);
@@ -93,5 +117,24 @@ public class UserServiceTest {
     private void checkLevel(User user, Level expectedLevel) {
         User userUpdate = userDao.get(user.getId());
         assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+
+    @Test
+    public void upgradeAllOrNothing() throws SQLException {
+        UserService testUserService = new TestUserService(users.get(3).getId());
+        testUserService.setUserDao(this.userDao);
+        testUserService.setDataSource(this.dataSource);
+        userDao.deleteAll();
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        try {
+            testUserService.upgradeLevels();
+            fail("Test UserServiceException expected");
+        } catch (TestUserServiceException e) {
+        }
+
+        checkLevelUpgraded(users.get(1), false);
     }
 }
